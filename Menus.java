86d33fd67df4b8b4f0e2a0a7937e3c36334bf498
devsecVinicius <77497869+devsecVinicius@users.noreply.github.com>
2022-01-12 09:10:14 -0300

    Update Menus.java

--- Menus.java
+++ Menus.java
@@ -1,1 +1,28 @@
-
+public class Menus{
+	public static void printMenu(){
+		System.out.println("\n\n\t--------------------------");
+		System.out.println("\t  Player de Video/Musica");
+		System.out.println("\t--------------------------");
+		System.out.println("\n 1) Selecionar Midia");
+		System.out.println("\n 2) Ver Midia(s)");
+		System.out.println("\n 3) Adicionar Midia");
+		System.out.println("\n 666) Sair");
+		System.out.print("\n\n>>> ");
+	}
+	
+	public static void printMenuOp2(){
+		System.out.println("\n\n\t--------------------------");
+		System.out.println("\t\t  Midias");
+		System.out.println("\t--------------------------\n\n");
+	}
+	
+	public static void printMenuOp3(){
+		System.out.println("\n\n\t--------------------------");
+		System.out.println("\t  Adicionar Video/Musica");
+		System.out.println("\t--------------------------");
+		System.out.println("\n 1) Adicionar Musica");
+		System.out.println("\n 2) Adicionar Video");
+		System.out.print("\n\n>>> ");
+	}
+	
+}
