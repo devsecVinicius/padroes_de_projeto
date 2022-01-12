428cc2a30ac7666af2b3e16296a65373b529d1d2
devsecVinicius <77497869+devsecVinicius@users.noreply.github.com>
2022-01-12 16:19:31 -0300

    Update NullMidia.java

--- NullMidia.java
+++ NullMidia.java
@@ -1,1 +1,17 @@
-
+public class NullMidia extends MidiaAbstrata{
+	
+	public NullMidia(){
+		this.nome = "Midia nao encontrada";
+		this.extensao = "";
+	}
+	
+	@Override
+	public String getNome(){
+		return this.nome;
+	}
+	@Override
+	public String getExtensao(){
+		return this.extensao;
+	}
+	
+}
