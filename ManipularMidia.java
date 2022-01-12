377a5746dc86327078ca16d86624e0b6222dd016
devsecVinicius <77497869+devsecVinicius@users.noreply.github.com>
2022-01-12 09:09:36 -0300

    Update ManipularMidia.java

--- ManipularMidia.java
+++ ManipularMidia.java
@@ -1,1 +1,24 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class ManipularMidia{
+	InterfaceAdicionarMidia addMidia;
+	String nome;
+	
+	public ManipularMidia(InterfaceAdicionarMidia addMidia, String nome){
+		this.addMidia = addMidia;
+		this.nome = nome;
+	}
+	
+	public ManipularMidia(){
+		
+	}
+	
+	public void imprimirMidias(List<Midia> midias){
+		int i = 0;
+		for(i = 0; i < midias.size() ; ++i){
+			System.out.println("Nome = " + midias.get(i).nome+"."+midias.get(i).extensao);
+		}
+	}
+}
 
