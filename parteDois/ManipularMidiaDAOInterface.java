60fdd1dbdf3198281df2163c36af77c001cbb266
devsecVinicius <77497869+devsecVinicius@users.noreply.github.com>
2022-01-31 14:34:01 -0300

    Update ManipularMidiaDAOInterface.java

--- parteDois/ManipularMidiaDAOInterface.java
+++ parteDois/ManipularMidiaDAOInterface.java
@@ -1,1 +1,13 @@
+import java.util.ArrayList;
+import java.util.List;
 
+public abstract class ManipularMidiaDAOInterface{
+
+	protected InterfaceAdicionarMidia addMidia;
+	protected String nome;
+
+	public abstract void imprimirMidias(List<MidiaAbstrata> midias);
+	
+	public abstract MidiaAbstrata selecionarMidia(List<MidiaAbstrata> listaDeMidias, String nomeDaMidia);
+
+}
