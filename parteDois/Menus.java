29b0e1e7ae5f54c25d8f163da8198f45b4caf5a4
devsecVinicius <77497869+devsecVinicius@users.noreply.github.com>
2022-01-31 20:42:04 -0300

    Update Menus.java

--- parteDois/Menus.java
+++ parteDois/Menus.java
@@ -1,1 +1,58 @@
-
+public class Menus{
+	public static void printMenu(){
+		System.out.println("\n\n\t--------------------------");
+		System.out.println("\t  Player de Video/Musica");
+		System.out.println("\t--------------------------");
+		System.out.println("\n 1) Selecionar Midia");
+		System.out.println("\n 2) Ver Midia(s)");
+		System.out.println("\n 3) Adicionar Midia");
+		System.out.println("\n 4) Configuracoes");
+		System.out.println("\n 5) Dispositivos");
+		System.out.println("\n 6) Usuario");
+		System.out.println("\n 7) Sair");
+		System.out.print("\n\n>>> ");
+	}
+	
+	public static void printMenuOp1(){
+		System.out.println("\n\n\t--------------------------");
+		System.out.println("\t     Selecionar Midia");
+		System.out.println("\t--------------------------\n\n");
+	}
+	
+	public static void printMenuOp2(){
+		System.out.println("\n\n\t--------------------------");
+		System.out.println("\t\t  Midias");
+		System.out.println("\t--------------------------\n\n");
+	}
+	
+	public static void printMenuOp3(){
+		System.out.println("\n\n\t--------------------------");
+		System.out.println("\t  Adicionar Video/Musica");
+		System.out.println("\t--------------------------");
+		System.out.println("\n 1) Adicionar Musica");
+		System.out.println("\n 2) Adicionar Video");
+		System.out.println("\n 3) Outro tipo de Midia");
+		System.out.println("\n 4) Voltar");
+		System.out.print("\n\n>>> ");
+	}
+	
+	public static void printMenuOp4(){
+		System.out.println("\n\n\t--------------------------");
+		System.out.println("\t      Configuracoes");
+		System.out.println("\t--------------------------");
+		System.out.print("\n 1) Velocidade de reproducao");
+		System.out.print("\n 2) Qualidade de reproducao");
+		System.out.print("\n 3) Voltar");
+		System.out.print("\n\n>>> ");
+	}
+	
+	public static void printMenuOp5(){
+		System.out.println("\n\n\t--------------------------");
+		System.out.println("\t      Dispositivos");
+		System.out.println("\t--------------------------");
+		System.out.println("\n 1) Adicionar Dispositivo");
+		System.out.println("\n 2) Remover Dispositivo");
+		System.out.println("\n 3) Ver Dispositivos");
+		System.out.print("\n\n>>> ");
+	}
+}
