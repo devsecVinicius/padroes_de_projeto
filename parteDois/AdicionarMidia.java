572338945561e5953b504e6958571e1217bc1e8c
devsecVinicius <77497869+devsecVinicius@users.noreply.github.com>
2022-02-02 20:43:43 -0300

    Update AdicionarMidia.java

--- parteDois/AdicionarMidia.java
+++ parteDois/AdicionarMidia.java
@@ -1,1 +1,14 @@
-
+public class AdicionarMidia implements InterfaceAdicionarMidia{
+	
+	private String extensao;
+	
+	public AdicionarMidia(String extensao){
+		super();
+		this.extensao = extensao;
+	}
+	
+	public Midia adicionar(String nome){
+		Midia midia = new Midia(nome, extensao);
+		return midia;		
+	}
+}
